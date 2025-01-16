ea35f5c997caee18ab6f2926d7ca438c914562f6
Jonathan Sigrist <jonathan.sigrist@xitaso.com>
2025-01-16 17:35:27 +0100

    basic visual structure and data loading for the submodel

--- SoftwareNameplateVisualizations.tsx
+++ SoftwareNameplateVisualizations.tsx
@@ -2,25 +2,30 @@ import { SubmodelVisualizationProps } from 'app/[locale]/viewer/_components/subm
 import { useLocale, useTranslations } from 'next-intl';
 import { findValueByIdShort } from 'lib/util/SubmodelResolverUtil';
 import Link from 'next/link';
+import { Fragment } from 'react';
 
 export function SoftwareNameplateVisualizations({ submodel }: SubmodelVisualizationProps) {
     const t = useTranslations('user-plugins.submodels.SoftwareNameplate');
     const locale = useLocale();
+    // const valueByIdShort = submodelValueMapper(submodel, locale);
+    //
+    // const link = valueByIdShort('URIOfTheProduct');
 
     const link = findValueByIdShort(submodel.submodelElements, 'URIOfTheProduct', locale) ?? '';
     const manufacturer = findValueByIdShort(submodel.submodelElements, 'ManufacturerName', locale) ?? '';
-
-    // const fullDescriptionElement = elements.find(el => el.idShort == 'ManufacturerProductDescription');
-    // const fullDescriptionDescription = getTranslationText(fullDescriptionElement as LangStringNameType[], intl)
+    const description = findValueByIdShort(submodel.submodelElements, 'ManufacturerProductDescription', locale) ?? '';
+    const version = findValueByIdShort(submodel.submodelElements, 'Version', locale) ?? '';
+    const releaseDate = findValueByIdShort(submodel.submodelElements, 'ReleaseDate', locale) ?? '';
 
     return (
         <>
             <h1>
-                Welcome to Mnestix by <Link href={link}>{manufacturer}</Link>
+                {t('welcome')} <Link href={link}>{manufacturer}</Link>
             </h1>
-            <h3>Asset Administration Shell made easy!</h3>
+            <h3>{t('slogan')}</h3>
+            <p>{description}</p>
             <img src="https://www.hubspot.com/hs-fs/hubfs/linkedin-summary-examples-4.jpg?width=595&height=400&name=linkedin-summary-examples-4.jpg" />
+            <h6>{t('versionInfo', { version: version, releaseDate: releaseDate })}</h6>
         </>
     );
-    return <p>{`${t('title')}: ${submodel.idShort}`}</p>;
 }
